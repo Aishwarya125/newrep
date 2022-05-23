1d4c21dcbca26f26b8361b11372ddd798c403807
Aishwarya125 <aishwaryaharini@gmail.com>
2022-05-23 14:30:44 +0530

    done

---
+++ src/org/demo/Demo.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class Demo {
+
+public static void main(String[] args) {
+	int i=5;
+	if (i>10)
+		System.out.println("number is lesser");
+	else
+		System.out.println("number is greater");
+}
+	
+		
+
+	}
+
+
